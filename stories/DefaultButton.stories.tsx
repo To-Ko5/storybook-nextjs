6c43ce852d2982d344fbbf68d3d7f89a9dc228bd
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-12 21:30:40 +0900

    Add: addon-knobs

--- stories/DefaultButton.stories.tsx
+++ stories/DefaultButton.stories.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 import { DefaultButton } from "../components/DefalutButton";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Components/Button",
+  title: "Components/DefaultButton",
   component: DefaultButton,
+  decorators: [withKnobs],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof DefaultButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof DefaultButton> = (args) => (
-  <DefaultButton {...args} />
+  <DefaultButton label={text("label", "ボタン")} onClick={() => {}} />
 );
 
 export const Primary = Template.bind({});

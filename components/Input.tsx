42fd908421d90bbeab9fc3445f7c3aa86474a352
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-14 20:14:09 +0900

    Add: input stories

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,27 @@
 import { FC } from "react";
 
-export const Input = () => {
-  return <input type="text" />;
+type Props = {
+  color: string;
+  borderColor: string;
+  placeholder: string;
+  onFocus: () => void;
+};
+export const Input: FC<Props> = ({
+  color,
+  borderColor,
+  placeholder,
+  onFocus,
+}) => {
+  const style = {
+    color: color,
+    borderColor: borderColor,
+  };
+  return (
+    <input
+      style={style}
+      placeholder={placeholder}
+      onFocus={onFocus}
+      type="text"
+    />
+  );
 };

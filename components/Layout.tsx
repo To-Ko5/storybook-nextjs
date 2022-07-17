3e412b7c50fa2747d3ccb96e26ce521a9b1e33f1
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-17 10:58:28 +0900

    Add: Layout components

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from 'react'
+import { Header } from './Header'
+
+type Props = {
+  children: ReactNode
+}
+const Layout: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default Layout
